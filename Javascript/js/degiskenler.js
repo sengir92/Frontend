9a4dc87b1fdab51e73c8a7cb010bc6da56c6f2f1
sengir92 <onursengir92@gmail.com>
2024-04-17 14:58:50 +0300

    update

--- Javascript/js/degiskenler.js
+++ Javascript/js/degiskenler.js
@@ -146,11 +146,12 @@
 // domIdWriteInfo('greeting', htmlInfo)
 // domIdWriteInfo('info', greeting("Lorem", "Ipsum"))
 
-// let counter = 0
+// Counter İşlemleri (Fonksiyonlar Bölüm Sonu Egzersizi) ve (Local Storage Bölüm Sonu Egzersizi)
+
+// let counter = localStorage.getItem('counter') ? Number(localStorage.getItem('counter')) : 0
 // let counterDOM = document.querySelector("#counter")
 // let increaseDOM = document.querySelector("#increase")
 // let decreaseDOM = document.querySelector("#decrease")
-
 
 // counterDOM.innerHTML = counter
 
@@ -158,10 +159,278 @@
 // decreaseDOM.addEventListener("click",clickEvent)
 
 // function clickEvent() {
-//     console.log(this.id)
-//     if(this.id == "increase") {
-//         counterDOM.innerHTML = counter +=1
-//     }else {
-//         counterDOM.innerHTML = counter -=1
+//     this.id == "increase" ? counter +=1 : counter -=1
+//     localStorage.setItem('counter',counter)
+//     counterDOM.innerHTML = counter
+// }
+
+//Local Storage İşlemleri
+
+// //Kompleks yapıyı Json'la stringe çevirme
+// let user = {userName: "onurşengir", isActive: true}
+// localStorage.setItem('userInfo',JSON.stringify(user))
+
+// //Stringe döndürülmüş yapıyı tekrar geri alma
+// let userInfo = localStorage.getItem("userInfo")
+// userInfo = JSON.parse(userInfo)
+// console.log(userInfo)
+
+//Form İşlemleri
+
+// let formDom = document.querySelector("#userForm")
+// formDom.addEventListener('submit', formSubmit)
+
+
+// function formSubmit(event) {
+//     event.preventDefault() //default işlemi engelleme
+//     console.log("islem gerçekleşti")
+
+//     let scoreInputDOM = document.querySelector('#score')
+//     console.log(scoreInputDOM.value)
+//     localStorage.setItem('score', scoreInputDOM.value)
+// }
+
+//Form Bölüm Sonu Egzersizi 
+
+// let userFormDOM = document.querySelector('#userForm')
+// userFormDOM.addEventListener('submit', formHandler)
+// const alertDOM = document.querySelector('#alert')
+
+// const alertFunction = (title,message, className = "warning") => `
+// <div class="alert alert-${className} alert-dismissible fade show" role="alert">
+//   <strong>${title}</strong> ${message}
+//   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+// </div>
+// `
+
+// function formHandler(event) {
+//     event.preventDefault()
+//     const USER_NAME = document.querySelector("#username")
+//     const SCORE = document.querySelector("#score")
+   
+//     if(USER_NAME.value && SCORE.value ) {
+//     addItem(USER_NAME.value, SCORE.value) //gönderdikten sonra sıfırladık
+//     USER_NAME.value = ""
+//     SCORE.value = ""
+//     } else {
+//         alertDOM.innerHTML = alertFunction(
+//             "Head info",
+//             "Enter incomplete information",
+//             "danger")
 //     }
 // }
+
+// let userListDOM = document.querySelector('#userList')
+
+
+// const addItem = (userName, score) => {
+//     let liDOM = document.createElement('li')
+//     liDOM.innerHTML = `${userName} 
+//                     <span class="badge bg-primary rounded-pill">${score} </span>`
+//     liDOM.classList.add('list-group-item' , 'd-flex' , 'justify-content-between' , 'align-items-center')
+//     userListDOM.append(liDOM)
+// }
+
+
+//Array İşlemleri
+
+// //sona öge eklemek -> push
+// items.push(50)
+// console.log("50 : " , items)
+
+// //başa öge eklemek -> unshift
+// items.unshift(5)
+// console.log("5 : " , items)
+
+// //sondaki ögeyi çıkarma -> pop
+// let lastItem = items.pop() // son elemani lastItem içerisine ekledik
+// console.log(lastItem,items)
+
+// //baştaki ögeyi çıkarma -> shift
+// let firstItem = items.shift() // ilk elemani firstItem içerisine ekledik
+// console.log(firstItem,items)
+
+// let items = [1, 2, 3, 4, 5]
+
+// let femaleUsers = ["Ayse", "Hulya", "Merve"]
+// let maleUsers = ["Ahmet", "Hasan", "Mehmet"]
+
+// items.unshift(femaleUsers)
+
+// items.push(maleUsers)
+// console.log(items)
+
+// //Array içinde öge ayırmak ->splice
+// let newItems = items.splice(1,5)
+// console.log(newItems)
+
+// //indeks bulma
+// items.unshift("lorem")
+// items.push("ipsum")
+// console.log(items.indexOf("ipsum"))
+
+// //Array Kopyalamak ->slice
+// let copyItems = items.slice()
+
+// let es6Items = [...items] //es6 ve sonrasinda gelen kopyalama işlemi
+// console.log(es6Items)
+
+// //Arrayleri Birleştirme
+// let allUsers = [...femaleUsers,...maleUsers] //es6 array birleştirme
+// console.log(allUsers)
+
+// let users = maleUsers.concat(femaleUsers) //concat() birleştirme
+// console.log(users)
+
+
+// allUsers.splice(allUsers.length - 1,0,"Melisa") //istediğimiz index bilgisine öge eklemek
+// console.log(allUsers)
+
+// //Arrayleri Sıralama
+
+// console.log(allUsers.sort()) //sıralama yapmak için
+
+// console.log(newItems.reverse()) //tersten sıralama yapmak için
+
+// let numbArr = [8, 26, 100 , 4, -10, -1000]
+// console.log(numbArr.sort((a,b) => {return a-b})) // sayılar içeren bir arrayde doğru sıralama yapmak için içerdeki fonk. gerekli yoksa olmuyor 
+
+
+// let userArr = [
+//     {id: 2, username : "rainman"},
+//     {id: 3, username : "white_pearl"},
+//     {id: 1, username : "Excalibur45"}
+// ]
+// console.log(userArr.sort((a,b) => {return a.id-b.id})) //içerdeki fonk idye göre sıralar
+// console.log(userArr.sort((a,b) => {return a.username.localeCompare(b.username)})) //içerdeki. fonk. username göre sıralar
+
+//REDUCE() METHODU -> reduce dizi içindeki her elemanı tek bir sonuç değerine indirger
+
+// let numbers = [1 ,2, 3, 5]
+// let total = numbers.reduce(function(acc,number) {return acc+number})
+// console.log(total)
+
+// let basket = [
+//     {
+//         name : "iPhone 8",
+//         price: 7000
+//     },
+//     {
+//         name: "Macbook Pro",
+//         price: 14000
+//     },
+//     {
+//         name: "Harman/Kardon",
+//         price: 2500
+//     }
+// ]
+
+// let sum = basket.reduce((acc,item) => acc+item.price, 0)
+// console.log(sum) 
+
+// let names = ["Tayfun", "Mehmet", "Ahmet", "Gökhan", "Tayfun", "Ahmet", "Tayfun"]
+// let countedNames = names.reduce((allNames,name) => {
+//         if(name in allNames) {
+//             allNames[name]++
+//         }else{
+//             allNames[name] = 1
+//         }
+//         return allNames
+//     }, {})
+
+//     console.log(countedNames)
+
+//(...) OPERATÖRÜ
+
+// let numbers = [10,20,30,40]
+// function add(a,b,c,d) {
+//     console.log(a+b+c+d)
+// }
+// add(...numbers)
+
+//DÖNGÜLER
+
+//FOR DÖNGÜSÜ
+
+// let users = ["Lorem", "Ipsum", "Dolor",]
+
+// const userListDOM  = document.querySelector('#userList')
+
+// for(let index = 0; index<users.length; index++) {
+//     const liDOM = document.createElement('li')
+//     liDOM.innerHTML = users[index]
+//     userListDOM.appendChild(liDOM)
+// }
+
+//BREAK VE CONTINUE
+
+// const LOREM_LIST = ["lorem", "ipsum", "dolor", "amet", "consectetur", "adipisicing", "elit"]
+
+// let counter = 0
+
+// for(;counter < 10; counter++) {
+//     if(counter === 5) {break}
+//     console.log(counter)
+// }
+
+// for(;counter < 10; counter++) {
+//     if(counter === 5) {continue}
+//     console.log(counter)
+// }
+
+// const UL_DOM = document.querySelector('#userList')
+
+// let index = 0
+
+// for(; index < LOREM_LIST.length;index++) {
+//     if(LOREM_LIST[index] === "dolor") {continue}
+//     let LI_DOM = document.createElement('li')
+//     LI_DOM.innerHTML = LOREM_LIST[index]
+//     UL_DOM.append(LI_DOM)
+// }
+
+//FOR EACH
+
+// const PRODUCTS = ["Laptop","Phone","Speaker","PC","Server","Mouse","Keyboard"]
+
+// PRODUCTS.forEach((product,index,array) => console.log(array[index]))
+
+// const userListDOM = document.querySelector('#userList')
+// PRODUCTS.forEach((item) => {
+//     const liDOM = document.createElement('li')
+//     liDOM.innerHTML=item
+//     userListDOM.append(liDOM)
+// })
+
+//Filter Kullanımı
+
+// const PRODUCTS = ["Mic","Cable","Speaker","Desktop PC","Server","Mouse","Keyboard"]
+
+// const NEW_PRODUCTS = PRODUCTS.filter(item => item.length > 5)
+// console.log(NEW_PRODUCTS)
+
+// const USERS = [
+//     {fullName: "Ayse Sumer", isActive:false},
+//     {fullName: "Ahmet Urgan", isActive:true},
+//     {fullName: "Asya Basar", isActive:true},
+//     {fullName: "Aksel Durmaz", isActive:false}
+// ]
+
+// const ACTIVE_USERS = USERS.filter(user => user.isActive)
+// console.log(ACTIVE_USERS)
+
+//Map Kullanımı
+
+const USERS = ["AYSE", "MehMet", "TugCE", "AkSEL"]
+
+const NEW_USERS = USERS.map(user => user.toLowerCase())
+console.log(NEW_USERS)
+
+const USERS_OBJ = USERS.map(item => 
+    {
+        return {userName : item, shortName : `${item[0]}.`,newName :`${item[0].toUpperCase()}${item.slice(1).toLowerCase()}` 
+    }
+    }
+)
+
+console.log(USERS_OBJ)
