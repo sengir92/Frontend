4e649dbab96ca59e76dddedb86b04df68153638e
sengir92 <onursengir92@gmail.com>
2024-04-23 22:42:20 +0300

    Apple Project

--- Javascript/js/degiskenler.js
+++ Javascript/js/degiskenler.js
@@ -435,3 +435,12 @@
 
 // console.log(USERS_OBJ)
 
+
+//Fetch API
+// fetch("settings.json").then(
+//     response => {return response.json()}
+// ).then(responseJson => {
+//     console.log(responseJson)
+//     console.log(responseJson.userName)
+// })
+
